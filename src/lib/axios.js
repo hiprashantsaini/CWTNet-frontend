f1094af3d4d2797e3632c8bc3449008eb2b424c1
Prashant <prashantsaini4449@gmail.com>
2026-02-05 23:42:02 +0530

    code updated

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-	baseURL: import.meta.env.MODE === "development" ? "https://cwt-net-backend.vercel.app/api/v1" : "/api/v1",
+	baseURL:"https://cwt-net-backend.vercel.app/api/v1",
 	withCredentials: true,
 });
